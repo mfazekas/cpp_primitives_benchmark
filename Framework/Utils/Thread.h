490938d278b995391c516538b1378c0865d497eb
Miklós Fazekas <mfazekas@szemafor.com>
2010-04-10 09:56:24 +0200

    Fixed thread issue

--- Framework/Utils/Thread.h
+++ Framework/Utils/Thread.h
@@ -3,7 +3,7 @@
 #include <pthread.h>
 #include <stdio.h>
 
-#define PTHREAD_CHECK(res) if (int err = res) { fprintf(stderr,#res" failed:%d",err); }
+#define PTHREAD_CHECK(res) if (int err = res) { fprintf(stderr,#res" failed:%d",err); exit(1); }
 template <typename Param>
 class Thread {
 public:
@@ -33,10 +33,12 @@ public:
         while(!in_wait_for_start) {}
     }
     void join() {
-        void* result = 0;
-        pthread_join(thread,&result);
-        started = false;
-        thread = 0;
+        if (thread) {
+          void* result = 0;
+          pthread_join(thread,&result);
+          started = false;
+          thread = 0;
+        }
     }
     virtual ~Thread() {
         if (started) join();
